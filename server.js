162a42332e882eaa23bff171355331d16466e06c
hsangalli <habyte@gmail.com>
2017-04-24 17:24:23 -0300

    Reset no projeto

--- server.js
+++ server.js
@@ -1,40 +1,15 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
 
-var database = {};
-var kanban = {};
+var kanban = {title: '', tasks: {todo: [], doing: [], done: []}}
 
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
-  res.render('index');
-});
+  res.send('Index')
+})
 
-app.get('/:kanban', (req, res) => {
-  res.render('kanban', {tasks: kanban.tasks.todo});
-});
-
-app.post('/enter', (req, res) => {
-  kanban = {
-    title: req.body.title,
-    tasks: {
-      todo: [],
-      doing: [],
-      done: []
-    }
-  };
-  res.redirect('/' + kanban.title);
-});
-
-app.post('/add', (req, res) => {
-  kanban.tasks.todo.push(req.body.task);
-  res.redirect('/' + kanban.title);
-});
-
-
-app.listen(3000, () => {
-  console.log('Server rodando em http://localhost:3000/');
-});
+app.listen(1234, () => {
+  console.log('Server rodando em http://localhost:1234/')
+})
