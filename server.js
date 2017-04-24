511a1312bfda1b2f69b5fa29231d739645a059b7
hsangalli <habyte@gmail.com>
2017-04-24 17:37:42 -0300

    Passos iniciais do MongoDB

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
 
-var kanban = {title: '', tasks: {todo: [], doing: [], done: []}}
+var db
+
+MongoClient.connect('mongodb://<dbuser>:<dbpassword>@ds117251.mlab.com:17251/kanbans', (err, database) => {
+	if (err) return console.log(err)
+	db = database
+	app.listen(3000, () => {
+		console.log("listening on 3000")
+	})
+})
 
 app.use(bodyParser.urlencoded({extended: true}))
+
+app.set('view engine' , 'ejs')
 
 app.get('/', (req, res) => {
   res.send('Index')
 })
 
-app.listen(1234, () => {
-  console.log('Server rodando em http://localhost:1234/')
+app.listen(3000, () => {
+  console.log('Server rodando em http://localhost:3000/')
 })
