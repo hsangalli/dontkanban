4df48b66ae8e9aeebb1eb55fdf7245a98582b839
Henrique <habyte@gmail.com>
2017-04-25 00:53:37 -0300

    Segundo reset do projeto

--- server.js
+++ server.js
@@ -1,26 +1,21 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const MongoClient = require('mongodb').MongoClient
 
-var db
-
-MongoClient.connect('mongodb://<dbuser>:<dbpassword>@ds117251.mlab.com:17251/kanbans', (err, database) => {
-	if (err) return console.log(err)
-	db = database
-	app.listen(3000, () => {
-		console.log("listening on 3000")
-	})
-})
+var database = []
 
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.set('view engine' , 'ejs')
 
 app.get('/', (req, res) => {
-  res.send('Index')
+  
+})
+
+app.get('/:kanban', (req, res) => {
+
 })
 
 app.listen(3000, () => {
-  console.log('Server rodando em http://localhost:3000/')
+  console.log('Servidor rodando em http://localhost:3000/')
 })
