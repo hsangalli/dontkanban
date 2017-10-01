8c516d340ed7c1b5002f258457fdf4de2c538610
hsangalli <habyte@gmail.com>
2017-09-30 23:01:24 -0300

    Expect to equal

--- test/example_test.js
+++ test/example_test.js
@@ -1,8 +1,8 @@
-const expect = require('chai').expect
-const example = require('../src/example')
+const expect = require('chai').expect;
+const example = require('../src/example');
 
 describe('Example', function () {
   it('#say() should return Hello World', function () {
-    expect(example.say()).to.be.equal('Hello World');
-  })
+    expect(example.say()).to.equal('Hello World');
+  });
 });
