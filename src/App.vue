27616cf7ad7ada0e09b2c9e85257e41eef008bc0
Henrique <habyte@gmail.com>
2017-12-29 19:21:50 -0200

    Added home page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -12,12 +11,23 @@ export default {
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  font-family: 'Open Sans';
+  height: 100vh;
+  width: 100vw;
+}
+
+#app h1 {
+  font-size: 2.5em;
+}
+
+#app h4 {
+  font-size: 1.25em;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="home">
+    <header>
+      <h1>Don't Kanban</h1>
+      <h4>Developed by <a href="https://github.com/hsangalli" target="_blank">@hsangalli</a></h4>
+    </header>
+    <div>
+      <input type="text" placeholder="Enter your kanban's title" autofocus>
+      <button type="button">Go!</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style>
+#home {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: 80%;
+  justify-content: center;
+  width: 100%;
+}
+
+#home header {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 100%;
+  height: 15%;
+}
+
+#home div {
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  width: 100%;
+  height: 10%;
+}
+
+#home div input {
+  border: 1px solid rgba(0,0,0,0.15);
+  border-radius: 4px;
+  font-size: 1.1em;
+  outline: none;
+  margin-right: 5px;
+  padding: 15px 10px;
+}
+
+#home div button {
+  background-color: transparent;
+  border: 1px solid rgba(0,0,0,0.15);
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 1.1em;
+  outline: none;
+  padding: 15px 20px;
+}
+</style>
